93da7dccdd976929af088b3ff07f4b303f9fcb16
riyasaaqil25-crypto <riyasaaqil25@gmail.com>
2026-05-30 16:16:21 +0100

    Add files via upload

---
+++ type casting.py
@@ -0,0 +1,21 @@
+name = "penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+print("name :", name)
+print("Data type name is", type(name))
+
+print("Age :", age)
+print("Data type age is", type(age))
+
+print("is_student :", is_student)
+print("Data type of is_student is", type(is_student))
+
+print("Weight :", weight)
+print("Data type weight is", type(weight))
+
+print("\n After type casting....")
+age = str(age)
+print(age)
+print("Data type of age is", type(age))
