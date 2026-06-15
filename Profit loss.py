2076fd8d83e5993e44044540c1c0bdd606b5a82c
riyasaaqil25-crypto <riyasaaqil25@gmail.com>
2026-06-15 20:31:43 +0100

    Add files via upload

---
+++ Profit loss.py
@@ -0,0 +1,8 @@
+actual_cost = float(input("Please enter the actual product price: "))
+sale_amount = float(input("Please enter the sales amount: "))
+
+if (sale_amount > actual_cost):
+    amount = sale_amount - actual_cost
+    print("Total profit = {0}".format(amount))
+else:
+    print("No profit!!!")
