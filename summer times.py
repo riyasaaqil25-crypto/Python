c16620b647d75b3212cabb866c78ba57f172832f
riyasaaqil25-crypto <riyasaaqil25@gmail.com>
2026-06-20 12:18:20 +0100

    Add files via upload

---
+++ summer times.py
@@ -0,0 +1,7 @@
+
+temperature = float(input("Enter the current temperature in Celsius: "))
+
+if temperature >= 20:
+    print("It is hot You can wear light or soft clothes.")
+else:
+    print("It is still a bit chilly.   wear with your jacket and pullover.")
