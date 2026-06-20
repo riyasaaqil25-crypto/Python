f0b3879ef47624dae2b43d37d9f8769c82b9dc6f
riyasaaqil25-crypto <riyasaaqil25@gmail.com>
2026-06-20 16:01:47 +0100

    Add files via upload

---
+++ NOT operator.py
@@ -0,0 +1,22 @@
+a = 10 
+b = 12
+c = 12
+
+print(not(a ==b ))
+
+a = "python" 
+b = "coding"
+
+if not (a == b):
+    print(a, 'and', b, 'are diffrent.')
+
+a = 4
+b = 5
+
+if not ((a == 1) == (b == 5)):
+ print('hello')
+
+ a = int(input("Enter a number: "))
+
+ if not (a % 2 == 0):
+    print(a, "is an odd number")
