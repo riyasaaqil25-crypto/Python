8a22f07d60ba4744e4207a3ff23bc061407bc546
riyasaaqil25-crypto <riyasaaqil25@gmail.com>
2026-06-26 23:16:48 +0100

    Add files via upload

---
+++ Congratulation message.py
@@ -0,0 +1,11 @@
+text = input("Enter a string: ")
+print("Original string:", text)
+print("Length of string:", len(text))
+print("Uppercase:", text.upper())
+print("Lowercase:", text.lower())
+print("First character:", text[0])
+print("Lact character:", text[-1])
+
+
+result = text + " - Welcome to python!"
+print("concatenated string:", result )
